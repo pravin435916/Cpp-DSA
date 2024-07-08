9132b2b1c5b65834908532b5de92ad8783286e1e
pravin nandankar <pravinnandankar03@gmail.com>
2024-07-08 22:32:37 +0530

    binary Search Added

---
+++ BinarySearch/sqareRoot.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0  || x == 1) return x;
+        int st = 1;
+        int end = x;
+        long long mid =-1;
+        while(st <= end) {
+            mid = st + (end - st) / 2;
+            if(mid*mid > x) {
+                end = mid-1;
+            }else if(mid*mid < x) {
+                st = mid+1;
+            }else {
+                return mid;
+            }
+        }
+        return end;
+    }
+};
