8cb4dcefa10c14f30883f5f4e754e406bbd77fef
pravin435916 <pravinnandankar03@gmail.com>
2025-07-03 17:06:54 +0530

    Added:TC

--- Recursion/braces/generateallbraces.cpp
+++ Recursion/braces/generateallbraces.cpp
@@ -2,20 +2,23 @@
 #include <string>
 #include <vector>
 using namespace std;
-void solve(string cur,int n) {
-    if(cur.length() == 2*n) {    //in pairs mean 2*n
-        cout<<cur<<endl;
+void solve(string cur, int n)
+{
+    if (cur.length() == 2 * n)
+    { // in pairs mean 2*n
+        cout << cur << endl;
         return;
     }
     cur += '(';
-    solve(cur,n);
-    
+    solve(cur, n);
+
     cur.pop_back();
-    
-    cur+=')';
-    solve(cur,n);
+
+    cur += ')';
+    solve(cur, n);
 }
-int main() {
-    solve("",2);
+int main()
+{
+    solve("", 2);
     return 0;
 }
